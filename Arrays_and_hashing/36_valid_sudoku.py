b9a0d9950c22f271361a1757ad892bae9e9a3f91
Pedro Mateus <pedropmateus91@gmail.com>
2024-05-05 19:11:38 +0100

    Valid sudoku

--- Arrays_and_hashing/36_valid_sudoku.py
+++ Arrays_and_hashing/36_valid_sudoku.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from typing import List
 
 
@@ -15,7 +16,28 @@ board = [
 
 
 def isValidSudoku(board: List[List[str]]) -> bool:
-    pass
+    cols = defaultdict(set)
+    rows = defaultdict(set)
+    squares = defaultdict(set)  # key =(r/3, c/3)
+
+    for r in range(9):
+        for c in range(9):
+            if board[r][c] == ".":
+                continue
+
+            if (
+                board[r][c] in rows[r]  # se ja existe na linha
+                or board[r][c] in cols[c]  # se ja existe na coluna
+                or board[r][c]
+                in squares[(r // 3, c // 3)]  # se ja existe neste quadrado 3x3
+            ):
+                return False
+
+            rows[r].add(board[r][c])
+            cols[c].add(board[r][c])
+            squares[(r // 3, c // 3)].add(board[r][c])
+
+    return True
 
 
 print(isValidSudoku(board))
