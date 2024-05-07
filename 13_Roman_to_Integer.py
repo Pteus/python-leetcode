c566cc9a1297087153dead25c9911a3c435206a7
Pedro Mateus <pedropmateus91@gmail.com>
2024-05-07 16:35:04 +0100

    13. Roman to Integer

---
+++ 13_Roman_to_Integer.py
@@ -0,0 +1,21 @@
+s = "MCMXCIV"
+
+"""
+    largest to smallest - decreasing: add them up
+    smaller before larger - increasing order: subtract smaller
+"""
+
+
+def romanToInt(s: str) -> int:
+    roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    res = 0
+    for i in range(len(s)):
+        if (i + 1) < len(s) and roman[s[i]] < roman[s[i + 1]]:
+            res -= roman[s[i]]
+        else:
+            res += roman[s[i]]
+
+    return res
+
+
+print(romanToInt(s))
