3599ac64c9f04b66e7425bc23b509edce8b68df6
Pedro Mateus <pedropmateus91@gmail.com>
2024-05-09 09:46:48 +0100

    folder structure

---
+++ 66_plus_one.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+digits = [1, 2, 3]
+
+
+def plusOne(digits: List[int]) -> List[int]:
+    pass
+
+
+print(plusOne(digits))
--- misc/fizzbuzz.py
+++ misc/fizzbuzz.py
@@ -11,5 +11,19 @@ def fizzbuzz(n):
             print(i)
 
 
+def fizzbuzz_2(n):
+    result = ""
+    for i in range(1, n + 1):
+        if i % 3 == 0:
+            result += "Fizz"
+        if i % 5 == 0:
+            result += "Buzz"
+        if not (i % 3 == 0 or i % 5 == 0):
+            result += str(i)
+        result += "\n"  # Add a newline after each number or combination
+    return result
+
+
 # Example usage:
-fizzbuzz(15)
+# fizzbuzz(15)
+print(fizzbuzz_2(15))
