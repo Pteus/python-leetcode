c8606dc160b19b50fe53e83f512c64a53f132902
Pedro Mateus <pedropmateus91@gmail.com>
2024-05-31 22:48:08 +0100

    same tree algorithm

---
+++ NeetCode_pro/104. Maximum Depth of Binary Tree.py
@@ -0,0 +1,29 @@
+from collections import deque
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        q = deque()
+        depth = 0
+
+        if root:
+            q.append(root)
+
+        while q:
+            for _ in range(len(q)):
+                curr = q.popleft()
+                if curr.left:
+                    q.append(curr.left)
+                if curr.right:
+                    q.append(curr.right)
+            depth += 1
+
+        return depth
